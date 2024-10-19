ab76df3eb33eb9b8dbff36b0ca336ba64d61e747
João Victor de F. Lima <jvdfreitas@gmail.com>
2024-10-19 16:56:15 -0300

    Update more styled for component `ProductListing`

--- Digital_Store/src/components/ProductListing.jsx
+++ Digital_Store/src/components/ProductListing.jsx
@@ -7,26 +7,38 @@ const ProductListing = () => {
       <section style={{ backgroundColor: "#FFFFFF" }} className="w-7">
         <ul className="grid list-none p-0">
           {produtos.produtos.map((produto) => (
-            <li key={produto.id} className="col-12 md:col-4">
-              <div className="shadow-3 p-3 border-round-md">
+            <li
+              style={{ backgroundColor: "#F9F8FE" }}
+              key={produto.id}
+              className="col-12 md:col-4 cursor-pointer"
+            >
+              <div
+                style={{ backgroundColor: "#FFFFFF" }}
+                className="shadow-3 p-3 border-round-sm"
+              >
                 <div className="relative">
                   <img
-                    className="w-full"
+                    className="w-full h-15rem"
                     style={{ objectFit: "contain" }}
                     src={imagem}
                     alt={produto.titulo}
                   />
                   {(produto.id === 1 || produto.id === 2) && (
-                    <h6 style={{ backgroundColor: "#E7FF86" }} className="absolute top-0 left-0 py-1 px-3 border-round-3xl">
+                    <h6
+                      style={{ backgroundColor: "#E7FF86" }}
+                      className="absolute top-0 left-0 py-2 px-3 border-round-3xl"
+                    >
                       {produto.descontoPorcento}
                     </h6>
                   )}
                 </div>
-                <h4 style={{ opacity: 0.4 }}>{produto.categoria}</h4>
-                <h3 className="mb-0 text-overflow-ellipsis white-space-nowrap overflow-hidden">
+              </div>
+              <div className="">
+                <h4 style={{ opacity: 0.4 }} className="mb-0">{produto.categoria}</h4>
+                <h3 className="mb-0 pb-0 mt-3 text-overflow-ellipsis white-space-nowrap overflow-hidden">
                   {produto.titulo}
                 </h3>
-                <div className="flex gap-2">
+                <div className="flex gap-2 pt-0 mt-0">
                   <h2>
                     <del style={{ opacity: 0.3 }}>{produto.desconto}</del>
                   </h2>
--- Digital_Store/src/pages/ProductListingPage.jsx
+++ Digital_Store/src/pages/ProductListingPage.jsx
@@ -6,7 +6,7 @@ const ProductListingPage = () => {
     <>
       <div className="flex flex-column min-h-screen">
         {/* Header */}
-        <section style={{ backgroundColor: "#F9F8FE" }} className="flex-grow-1">
+        <section style={{ backgroundColor: "#F9F8FE", paddingRight: "11rem" }} className="flex justify-content-end">
           <ProductListing />
         </section>
         <section className="mt-auto">
